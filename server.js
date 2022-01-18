e91f9a71e72e44cee0bb2165323c0a13a1ce0b7f
kevinnayar <kevin.nayar@gmail.com>
2022-01-18 10:45:46 -0600

    refactor

--- server.js
+++
@@ -1,9 +0,0 @@
-var port = 3000;
-var serverUrl = '127.0.0.1';
-
-var connect = require('connect');
-var serveStatic = require('serve-static');
-
-// start server
-connect().use(serveStatic(__dirname)).listen(port, serverUrl);
-console.log(`✨ Starting web server at http://localhost:${port}`);
